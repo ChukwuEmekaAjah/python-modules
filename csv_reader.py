7129d56d25040fd3dabe06ce74f5293ffce42800
chukwuemekaajah <talk2ajah@gmail.com>
2020-02-18 21:27:26 +0100

    converted CSV reader module to class

--- csv_reader.py
+++ csv_reader.py
@@ -1,4 +1,44 @@
 import sys
+from os import linesep
+
+class CSVParser:
+	def __init__(self, file_path, separator = ','):
+		if not file_path.strip():
+			print("You must provide a valid file path")
+			return
+		self.file_path = file_path.strip()
+		self.separator = separator
+		self.data_table = []
+		self.file = None
+		try:
+			self.file = open(self.file_path, "r", encoding="utf-8")
+		except Exception as er:
+			print("There was an error trying to instantiate the parser")
+			print(er)
+
+	def toJSON(self, file_destination = 'to.json', fields = []):
+		if self.file:
+			object_keys = []
+			for line_number, line in enumerate(self.file.readlines()):
+				line = line.replace(linesep, '').split(self.separator)
+				if line_number == 0:
+					object_keys = fields if len(fields) > 0 else line 
+					continue
+				row = {}
+				for index,key in enumerate(object_keys):
+					row[key] = line[index]
+				print(row)
+				self.data_table.append(row)
+
+	@property 
+	def data(self):
+		return self.data_table
+
+parser = CSVParser("files/tech_funding.csv",",")
+parser.toJSON()
+#print(parser.data)
+
+"""
 def read_csv(file_path,separator = ',',fields={}):
 	if not file_path.strip():
 		print("You must provide a valid file path")
@@ -28,7 +68,7 @@ def read_csv(file_path,separator = ',',fields={}):
 
 	if __name__ == '__main__':
 		print(sys.argv)
-
-read_csv("files/tech_funding.csv",",")
+"""
+#read_csv("files/tech_funding.csv",",")
 
 
